61c29380fb4ee21f3b1f9956dace4b51d6acd95c
Pavel Ivanov <ast0r89@gmail.com>
2018-02-19 15:39:31 +0200

    add simple_webapp

--- simple_webapp.py
+++ simple_webapp.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 app.secret_key = '!@#$%^&*()QWERTYUIOP{'
 
+def check_logged_in() -> bool:
+    if 'logged_in' in session:
+        return True
+    return False
+
 
 @app.route('/')
 def hello() -> 'str':
@@ -12,7 +17,7 @@ def hello() -> 'str':
 
 @app.route('/login')
 def do_login() -> 'str':
-    session['logged_in'] = True
+    session['logged_in'] = 'True'
     return 'You are now logged in: ' + session['logged_in']
 
 
@@ -22,24 +27,26 @@ def do_logout() -> 'str':
     return 'You are now logged out'
 
 
-@app.route("status")
+@app.route("/status")
 def check_status() -> 'str':
     if 'logged_in' in session:
         return 'You are currently logged in'
     return 'You are NOT logged in'
 
 
-@app.route('page1')
+@app.route('/page1')
 def page1() -> 'str':
+    if not check_logged_in():
+        return 'You are NOT logged in'
     return 'This is Page1'
 
 
-@app.route('page2')
+@app.route('/page2')
 def page2() -> 'str':
     return 'This is page2'
 
 
-@app.route('page3')
+@app.route('/page3')
 def page3() -> 'str':
     return 'This is page 3'
 
